e33acf80c61831572715a72c9beb1c4fdc3303e4
aluketa <alex.luketa@gmail.com>
2016-05-12 19:04:55 +0100

    Skeleton implementation of pivot table demo component.

---
+++ src/main/resources/webroot/js/app/pivot-table-demo.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var PivotDemo = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Pivot table demo
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <PivotDemo />,
+    document.getElementById('demo')
+);
